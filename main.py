517024ceae3e900f00439420342a97b16ce1649e
ShavitRely <120562781+ShavitRely@users.noreply.github.com>
2023-04-25 09:45:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,20 @@ with open('../data/Hello.txt', 'r') as f:
         elements = str(res)
 with open('../results/result.txt', 'w') as f:
     f.write("This is the result: " + str(elements))
+
+try:
+    import sys
+    #Parameters
+    text_param = sys.argv[1]
+    list_parameter = sys.argv[2]
+    file_parameter = sys.argv[3] 
+    
+    with open('../results/result_param.txt', 'w') as f:
+        f.write("This is the Text param: " + text_param)
+        f.write("\nThis is the List param: " + list_parameter)
+        f.write("\nThis is the File param: " + file_parameter)
+
+except ImportError:
+    with open('../results/result_param.txt', 'w') as f:
+        f.write('Error')
+    print("To generate a result figure, please fix the code")
